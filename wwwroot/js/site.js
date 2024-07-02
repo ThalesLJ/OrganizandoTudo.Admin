c79ce89f4815f8e29c11134dac3a4da2766c0e64
Thales Lima <thales@inovaebiz.com.br>
2024-07-01 23:01:23 -0300

    Configuracao inicial do deploy com github action

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿<script>
+    $(document).ready(function () {
 
-// Write your JavaScript code.
+    });
+</script>
